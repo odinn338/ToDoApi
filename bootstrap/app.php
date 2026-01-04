814e106479e419f27464e3f574c92e6baac35861
odinn338 <xa00047@gmail.com>
2026-01-04 23:44:09 +0400

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -17,3 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$middleware->trustProxies(
+    '*',
+    Request::HEADER_X_FORWARDED_FOR  |
+        Request::HEADER_X_FORWARDED_HOST  |
+        Request::HEADER_X_FORWARDED_PORT  |
+        Request::HEADER_X_FORWARDED_PROTO
+);
